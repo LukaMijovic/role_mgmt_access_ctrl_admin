def5322070c44a410da0697e5a77ebae4ac7fb65
Luka Mijović <lukamij@telekom.rs>
2024-10-07 12:37:15 +0200

    added tabs and cards for registration and login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatTabsModule} from '@angular/material/tabs';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    MatTabsModule,
+    RegistrationFormComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/registration-form/registration-form.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+
+@Component({
+  selector: 'app-registration-form',
+  standalone: true,
+  imports: [
+    MatCardModule,
+  ],
+  templateUrl: './registration-form.component.html',
+  styleUrl: './registration-form.component.scss'
+})
+export class RegistrationFormComponent {
+
+}
